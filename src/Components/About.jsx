0fce3d17283dfccdbf3fd92d1e9d77203efc2731
Muneeba iqbal <ucpadpit0063@gmail.com>
2025-04-29 21:51:55 +0500

    new image

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,3 +1,4 @@
+
 export default function About() {
     return (
         <div className="py-16 bg-white">
@@ -5,7 +6,6 @@ export default function About() {
                 <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
                     <div className="md:5/12 lg:w-5/12">
                         <img
-                            src="/src/assets/images/startup.png"
                             alt="image"
                         />
                     </div>
